7dd4c628acde86ba15eeaf5ce57f17748dd6cbf5
Caiti Hensley <caiti@hampguest69-248.hampshire.edu>
2019-04-29 11:00:58 -0400

    dfkdjfkdjslfkdjfklsjdfkds

--- js/ambiance-retriever.js
+++ js/ambiance-retriever.js
@@ -1,9 +1,9 @@
-export updateBackground = function(query) { // location is the location of the story. eg. 'field'
+export updateBackground(query) { // location is the location of the story. eg. 'field'
     var file_name = "images/" + query["location"] + ".mp3";
     return {error: undefined, files: file_name};
 }
 
-export updateSound = function(query) {
+export updateSound(query) {
     var file_name = "sounds/" + query["location"] + ".jpeg";
     return {error: undefined, files: file_name};
 }
